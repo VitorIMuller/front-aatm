07ffbaedf188fbca1417529f716310af066a309a
vitor <vitor@imobia.app>
2023-10-08 10:14:24 -0300

    final commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { useState } from "react";
+import { useState } from "react"; 
 import { ptBR } from "@mui/x-date-pickers";
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
